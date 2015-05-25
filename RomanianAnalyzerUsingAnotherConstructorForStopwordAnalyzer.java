c8ad8f468388f3c8d45ed8ac15e49eb939808fb8
gabrielbudau <gabrielbudau@gmail.com>
2015-05-25 11:41:53 +0300

    Moddified the charset.

--- RomanianAnalyzerUsingAnotherConstructorForStopwordAnalyzer.java
+++ RomanianAnalyzerUsingAnotherConstructorForStopwordAnalyzer.java
@@ -1,21 +1,5 @@
 
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import java.io.IOException;
 import java.io.Reader;
@@ -43,19 +27,8 @@ public final class RomanianAnalyzerUsingAnotherConstructorForStopwordAnalyzer ex
   
   /** File containing default Romanian stopwords. */
   public final static String DEFAULT_STOPWORD_FILE = "stopwords.txt";
-  /**
-   * The comment character in the stopwords file.  
-   * All lines prefixed with this will be ignored.
-   */
-  private static final String STOPWORDS_COMMENT = "#";
+
   
-  /**
-   * Returns an unmodifiable instance of the default stop words set.
-   * @return default stop words set.
-   */
-  public static CharArraySet getDefaultStopSet(){
-    return DefaultSetHolder.DEFAULT_STOP_SET;
-  }
   
   /**
    * Atomically loads the DEFAULT_STOP_SET in a lazy fashion once the outer class 
